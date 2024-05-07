5dbe024f0a9631cb217488dd61ac2ce65e429ac2
Asmaa Adel <adelasmaa333@gmail.com>
2024-05-08 00:49:31 +0300

    update gui

--- gui/gui.py
+++ gui/gui.py
@@ -24,20 +24,21 @@ def compile_code():
     status_bar.config(text="Compiling...", fg="white", bg="blue")
    
     compiler = "..\src\out.exe ./output/temp.txt"
-    print("test")
     process = subprocess.Popen(compiler, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     out, _ = process.communicate()
 
     with open('./output/error_file.txt') as f:
-        print("test")
         contents = f.read()
+    
+    output_panel_quadruples.delete(1.0, tk.END)
+    output_panel.delete(1.0, tk.END)
+    output_panel.insert(tk.END, out.decode("utf-8"))
     if contents:    
-        output_panel.delete(1.0, tk.END)
-        output_panel.insert(tk.END, out.decode("utf-8"))
         status_bar.config(text="Error", fg="white", bg="red")
     else: 
-        output_panel.delete(1.0, tk.END)
-        output_panel.insert(tk.END, out.decode("utf-8"))
+        with open('./output/quadruples.txt') as f:
+            quadruples = f.read()
+        output_panel_quadruples.insert(tk.END, quadruples)
         status_bar.config(text="Compiled Successfully!", fg="white", bg="green")
 
 
@@ -53,6 +54,7 @@ file_menu.add_separator()
 file_menu.add_command(label="Exit", command=root.quit)
 menu_bar.add_cascade(label="File", menu=file_menu)
 root.config(menu=menu_bar)
+
 code_editor = tk.Text(root, wrap=tk.WORD)
 code_editor.pack(expand=True, fill=tk.BOTH)
 
@@ -62,7 +64,16 @@ compile_button.pack()
 status_bar = tk.Label(root, text="Ready", bd=1, relief=tk.SUNKEN, anchor=tk.W)
 status_bar.pack(side=tk.BOTTOM, fill=tk.X)
 
-output_panel = tk.Text(root, wrap=tk.WORD)
-output_panel.pack(expand=True, fill=tk.BOTH)
+second_section = tk.Frame(root)
+second_section.pack(expand=True, fill=tk.BOTH)
+
+output_panel = tk.Text(second_section, wrap=tk.WORD)
+output_panel.grid(row=0, column=0, sticky="nsew")
+
+output_panel_quadruples = tk.Text(second_section, wrap=tk.WORD)
+output_panel_quadruples.grid(row=0, column=1, sticky="nsew")
+
+second_section.grid_columnconfigure(0, weight=1)
+second_section.grid_columnconfigure(1, weight=1)
 
 root.mainloop()
